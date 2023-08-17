ad2c4c03e9762fb4d6048e515826512c55590f59
Yazmin Padilla <yazminpadilla@Yazmins-MacBook-Pro.local>
2023-08-17 14:56:49 -0700

    new change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './components/header.js';
 import Introduction from './components/introduction';
+import Sidebar from './components/sidebar';
+import About from './components/about';
+import Exper from './components/experience';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Sidebar />
       <Introduction/>
+      <div id="about" className="about-box">
+        <About/>
+      </div>
+      <Exper/>
     </div>
   );
 }
---
+++ my-app/src/components/about.js
@@ -0,0 +1,21 @@
+import '../App.css';
+import profileImage from '../IMG_3166.JPG'; 
+function About() {
+    return (
+      <div >
+        <h3 className="AboutMe">About Me</h3>
+        <div className="about-content">
+          <img src={profileImage} alt="Profile" className="about-image"/>
+          <div className="about-bio">
+            <h5>
+            I'm a senior Computer Science major at Stanford, proudly representing as a 
+            first-generation and low-income student. My journey into engineering and computer science began serendipitously; a chance meeting with a Latina software engineer in a cafe illuminated the boundless potential of engineering. Driven by a passion for education, 
+            creativity, social, and environmental justice, I'm eager to intertwine my passions with tech. When I'm not immersed in code, I indulge in baking, cherish time with my pet birds, or delve into the vibrant world of anime!
+            </h5>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+export default About
